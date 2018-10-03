220756118559d4f6cb5ce8abb1c63854b94919d2
Idan Izhaki <izhaki.idan@gmail.com>
2018-10-04 01:11:50 +0300

    Add update & delete routes

--- music/urls.py
+++ music/urls.py
@@ -6,5 +6,7 @@ app_name="music"
 urlpatterns = [
     path('', views.indexView.as_view(), name="index"),
     path('<pk>/', views.detailsView.as_view(), name="details"),
-    path('album/add/', views.AlbumCreate.as_view(), name="album-add")
+    path('album/add/', views.AlbumCreate.as_view(), name="album-add"),
+    path('album/<pk>/', views.AlbumUpdate.as_view(), name="album-update"),
+    path('album/<pk>/delete/', views.AlbumDelete.as_view(), name="album-delete")
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,8 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Album
+
 
 class indexView(generic.ListView):
     template_name="music/index.html"
@@ -16,3 +18,11 @@ class detailsView(generic.DetailView):
 class AlbumCreate(CreateView):
     model = Album
     fields = ['title', 'artist', 'year', 'image']
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['title', 'artist', 'year', 'image']
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
