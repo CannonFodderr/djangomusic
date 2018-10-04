c978af17df95af2e7a44068af25d6be04634aa55
Idan Izhaki <izhaki.idan@gmail.com>
2018-10-04 17:38:01 +0300

    Add edit & delete buttons to details template

--- music/urls.py
+++ music/urls.py
@@ -5,8 +5,8 @@ app_name="music"
 
 urlpatterns = [
     path('', views.indexView.as_view(), name="index"),
-    path('<pk>/', views.detailsView.as_view(), name="details"),
     path('album/add/', views.AlbumCreate.as_view(), name="album-add"),
-    path('album/<pk>/', views.AlbumUpdate.as_view(), name="album-update"),
+    path('album/<pk>/', views.detailsView.as_view(), name="details"),
+    path('album/<pk>/edit', views.AlbumUpdate.as_view(), name="album-update"),
     path('album/<pk>/delete/', views.AlbumDelete.as_view(), name="album-delete")
 ]
